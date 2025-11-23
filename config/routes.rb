7d69a880a25c752668f2636ea491a0547c7d94cc
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-11-23 20:44:42 +0900

    Deviseとコアリソースのルーティング設計を完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # 旅程に関するリソース（作成、一覧、詳細など）を定義
+  resources :trips do
+    # 旅程にネストする機能（会話履歴、スポット管理）を定義
+    resources :messages, only: [:create, :update, :destroy] # コメント投稿・編集・削除機能
+    resources :spots, only: [:create, :update, :destroy] # スポットの追加・編集機能
+    resources :checklists, only: [:index, :create, :update] # チェックリスト表示・管理機能
+    
+    # 共有設定画面
+    member do
+      get 'sharing' # 旅程共有設定画面へのルート (Req H)
+    end
+  end
+
+  # アプリケーションのルートパスを旅程一覧に設定
+  root "trips#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
