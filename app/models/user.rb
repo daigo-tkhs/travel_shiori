755d60ef64cffba967f5d3fbe1a7f645428ca05c
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-11-26 08:22:09 +0900

    fix: Userモデルにowned_tripsとtrip_usersアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 # app/models/user.rb
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  has_many :owned_trips, class_name: 'Trip', foreign_key: 'owner_id', dependent: :destroy
+  has_many :trip_users, dependent: :destroy
+  has_many :trips, through: :trip_users
+
 end
