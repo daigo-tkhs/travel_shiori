ae9aaf4af5d6927f368ed66fa819943db70f8c96
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-12-06 13:16:53 +0900

    fix: 本番環境の重複エラー解消のため create_table に force: true を追加

--- db/migrate/20251206032918_create_checklist_items.rb
+++ db/migrate/20251206032918_create_checklist_items.rb
@@ -1,6 +1,6 @@
 class CreateChecklistItems < ActiveRecord::Migration[7.1]
   def change
-    create_table :checklist_items do |t|
+    create_table :checklist_items, force: true do |t|
       t.string :name
       t.boolean :is_checked, default: false, null: false
       t.references :trip, null: false, foreign_key: true
