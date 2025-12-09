4318debf4edae7600bedb54daadf39ea15763739
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-12-09 17:23:44 +0900

    パスワードリセットメールのデザイン修正（TripConcierge仕様）

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,13 +19,12 @@ Devise.setup do |config|
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
-
+  config.mailer_sender = 'trip.concierge.contact@gmail.com'
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
