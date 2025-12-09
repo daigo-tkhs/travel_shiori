522fe7b94e326916a29400ce43e6ed2519a3b4b4
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-12-09 23:04:50 +0900

    Fix: Mailerでルーティングヘルパーが見つからない問題を解消 (@invitation_urlを生成)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
 
   include Rails.application.routes.url_helpers
-  
+
   def welcome
     @greeting = "Hi"
     mail to: params[:to], subject: "【TripConcierge】ようこそ！旅の準備を始めましょう"
@@ -10,8 +10,8 @@ class UserMailer < ApplicationMailer
   def invite_to_trip
     @trip = params[:trip]
     @inviter = params[:inviter]
-    @token = params[:invitation_token] # ★追加
-
+    @token = params[:invitation_token]
+    @invitation_url = invitation_accept_url(token: @token)
     mail(
       to: params[:to], 
       subject: "【TripConcierge】#{@inviter.nickname}さんから旅の招待状が届いています"
