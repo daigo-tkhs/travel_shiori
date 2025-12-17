a3772fcabdad742783ac4db92288de1864b8ceed
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-12-17 16:30:42 +0900

    Fix: JSファイルをHTMLで強制読み込みする最終手段を実行

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,11 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# ポイント: to: の指定で必ず ".js" を付けること
-pin "controllers/map_controller", to: "controllers/map_controller.js"
-pin "controllers/sortable_controller", to: "controllers/sortable_controller.js"
-pin "controllers/geocoding_controller", to: "controllers/geocoding_controller.js"
-
 # その他
 pin "sortablejs" 
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js"
