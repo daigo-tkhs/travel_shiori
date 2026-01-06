c12b1ec5b4bb3b78d9a658c2708798f0eda20f61
daigotechcareer-arch <daigo.tech.career@gmail.com>
2026-01-06 23:16:37 +0900

    Fix: LPのフッター非表示設定とFlashメッセージ表示ロジックの修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:top], raise: false
 
   def top
+    @hide_footer = true
     # routes.rbで制御済みのため、ここでのリダイレクト処理は不要です
   end
 end
